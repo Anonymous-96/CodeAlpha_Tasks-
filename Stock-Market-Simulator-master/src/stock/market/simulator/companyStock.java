84bb34f4499dead5a2ea742c7535a0994d041944
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:14:25 +0530

    Update companyStock.java

--- Stock-Market-Simulator-master/src/stock/market/simulator/companyStock.java
+++ Stock-Market-Simulator-master/src/stock/market/simulator/companyStock.java
@@ -2,8 +2,7 @@ package stock.market.simulator;
 
 public class companyStock extends stockProfile {
 
-    // Class constructor
-    public companyStock(String name, double m, double sPrice, double bPrice) {
+     public companyStock(String name, double m, double sPrice, double bPrice) {
         super(name, sPrice, bPrice, m);
 
     }

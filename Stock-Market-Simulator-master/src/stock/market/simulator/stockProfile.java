02f38d05c783f580ad20860424a218af1914ba98
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:18:15 +0530

    Update stockProfile.java

--- Stock-Market-Simulator-master/src/stock/market/simulator/stockProfile.java
+++ Stock-Market-Simulator-master/src/stock/market/simulator/stockProfile.java
@@ -9,16 +9,14 @@ public class stockProfile {
     private double margin;
     private int quantity;
 
-    // Class constructor for stock that has been bought 
-    public stockProfile(String pName, double sPrice, double bPrice, int quant) {
+     public stockProfile(String pName, double sPrice, double bPrice, int quant) {
         profileName = pName;
         buyPrice = bPrice;
         sellPrice = sPrice;
         quantity = quant;
     }
 
-    // Class constructor for all stocks when program is launched
-    public stockProfile(String pName, double sPrice, double bPrice, double m) {
+     public stockProfile(String pName, double sPrice, double bPrice, double m) {
         profileName = pName;
         buyPrice = bPrice;
         sellPrice = sPrice;
@@ -26,53 +24,43 @@ public class stockProfile {
         change = 0;
     }
 
-    // Method to retrieve the quatity of stocks bought
-    public int getQuantity() {
+     public int getQuantity() {
         return quantity;
     }
 
-    // Method to set the number of a stock that has been bought
-    public void setQuantity(int q) {
+     public void setQuantity(int q) {
         quantity = q;
     }
 
-    // Method to retrieve the profile name
-    public String getProfileName() {
+     public String getProfileName() {
         return profileName;
     }
 
-    // Method to retrieve the buy price
-    public double getBuyPrice() {
+     public double getBuyPrice() {
         return buyPrice;
     }
 
-    // Method to retrieve the sell price
-    public double getSellPrice() {
+     public double getSellPrice() {
         return sellPrice;
     }
 
-    // Method to set the buy price
-    public void setBuyPrice(double bPrice) {
+     public void setBuyPrice(double bPrice) {
         buyPrice = bPrice;
     }
 
-    // Method to set the sell price
-    public void setSellPrice(double sPrice) {
+     public void setSellPrice(double sPrice) {
         sellPrice = sPrice;
     }
 
-    // Method to get the margin
-    public double getMargin() {
+     public double getMargin() {
         return margin;
     }
 
-    // Method to set the change in current and previous rate
-    public void setChange(double c) {
+     public void setChange(double c) {
         change = c;
     }
     
-    // Method to get the change in current and previous rate
-    public double getChange() {
+     public double getChange() {
         return change;
     }
 }

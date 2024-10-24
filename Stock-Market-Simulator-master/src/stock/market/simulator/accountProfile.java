2e1b37829899ea9467b8d43f5b1e0cf767f15cfc
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:13:06 +0530

    Update accountProfile.java

--- Stock-Market-Simulator-master/src/stock/market/simulator/accountProfile.java
+++ Stock-Market-Simulator-master/src/stock/market/simulator/accountProfile.java
@@ -8,41 +8,34 @@ public class accountProfile {
     private double balance;
     private ArrayList<stockProfile> stocksBought;
 
-    // Class constructor
-    public accountProfile(String aName, double bal) {
+     public accountProfile(String aName, double bal) {
         accountName = aName;
         balance = bal;
         stocksBought = new ArrayList<>();
 
     }
 
-    // Method to retrieve the account name
-    public String getAccountName() {
+     public String getAccountName() {
         return accountName;
     }
 
-    // Method to set the account balance
-    public void setBalance(double b) {
+     public void setBalance(double b) {
         balance += b;
     }
 
-    // Method to retrieve the account balance
-    public double getBalance() {
+     public double getBalance() {
         return balance;
     }
 
-    // Method to add a stock once bought
-    public void addStock(stockProfile s) {
+     public void addStock(stockProfile s) {
         stocksBought.add(s);
     }
 
-    // Method to remove a stock once sold
-    public void removeStock(stockProfile s) {
+     public void removeStock(stockProfile s) {
         stocksBought.remove(s);
     }
 
-    // Method to retrieve all stocks bought
-    public ArrayList<stockProfile> getStocks() {
+     public ArrayList<stockProfile> getStocks() {
         return stocksBought;
     }
 }

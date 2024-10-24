03c42770e1c6a0b711d2b3c51f5617ca0a1187af
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:22:56 +0530

    Update UserInfo.java

--- Hotel Management System/src/I3/Classes/UserInfo.java
+++ Hotel Management System/src/I3/Classes/UserInfo.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package I3.Classes;
 
-/**
- *
- * @author Faysal Ahmed
- */
+ 
 public class UserInfo {
     
-   
-      
-    //USER PROPERTIES
     private int customer_id;
     private String name;
     private String address;

0831ada73fb1efdf6df4505737fbc2a481261a2d
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:31:47 +0530

    Update CustomCellRenderer.java

--- Hotel Management System/src/I3/TableModel/CustomCellRenderer.java
+++ Hotel Management System/src/I3/TableModel/CustomCellRenderer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ 
 package I3.TableModel;
 
 import java.awt.Color;
@@ -12,31 +8,14 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
 
-/**
- *
- * @author Faysal
- */
+ 
 public class CustomCellRenderer extends DefaultTableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
 
-        //System.out.println("workings");
-/*
-        
-        if (value.equals("<<"))
-        {
-            c.setBackground(new java.awt.Color(0, 0, 255));
-        }
-        else 
-        {
-            c.setBackground(table.getBackground());
-        }
-        
-        // for creating row header illusion
-  
-        */
+   
         if(column == 0)
         {
             

2791725ab4a373c8633fe1b62730805d933fa5c2
Anonymous-96 <ashashukla8795@gmail.com>
2024-10-24 12:16:10 +0530

    Update economyStock.java

--- Stock-Market-Simulator-master/src/stock/market/simulator/economyStock.java
+++ Stock-Market-Simulator-master/src/stock/market/simulator/economyStock.java
@@ -5,8 +5,7 @@ public class economyStock extends stockProfile {
     private final String country;
     private final double margin;
     
-    // Class constructor
-    public economyStock(String c, String name, double m, double sPrice, double bPrice) {
+     public economyStock(String c, String name, double m, double sPrice, double bPrice) {
         super(name, sPrice, bPrice, m);
         country = c;
         margin = m;
